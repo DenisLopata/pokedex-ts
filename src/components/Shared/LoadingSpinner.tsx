8fc4f2b6d6c7711b4721f47ebe9ef884b2b50506
Mladen Perišić <mladen.perisic@comminus.hr>
2021-05-17 20:31:35 +0200

    loading spinner takes in messages 🍡

--- src/components/Shared/LoadingSpinner.tsx
+++ src/components/Shared/LoadingSpinner.tsx
@@ -5,18 +5,21 @@ import loadingSpinner from "../../content/images/fidgetSpinnerLoader.gif";
 interface ILoadingSpinnerProperties {
     width: number;
     height: number;
+    subText: string;
 }
 
-export const LoadingSpinner = ({width, height}: ILoadingSpinnerProperties) => {
+export const LoadingSpinner = ({width, height, subText}: ILoadingSpinnerProperties) => {
 
 return (
+  <div>
 <img
               width={width}
               height={height}
               alt="loading..."
               src={loadingSpinner}
             ></img>
-
+            <div>{subText}</div>
+</div>
 );
 
 };
